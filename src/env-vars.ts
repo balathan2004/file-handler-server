ff42d4753d8e9dd174d9282f128e81e09c2ad945
light <balathan2004@gmail.com>
2025-02-13 15:46:27 +0530

    testing sample route

---
+++ src/env-vars.ts
@@ -0,0 +1,13 @@
+const minimalBlogConfig = {
+    apiKey: "AIzaSyBbJMasVNEjPiOENqVe8lxoPHR7sYCyYsM",
+    authDomain: "minimal-blog-99699.firebaseapp.com",
+    projectId: "minimal-blog-99699",
+    storageBucket: "minimal-blog-99699.appspot.com",
+    messagingSenderId: "687239234051",
+    appId: "1:687239234051:web:5ada35e185c43c18970436"
+  };
+
+
+  export {minimalBlogConfig};
+
+  //
